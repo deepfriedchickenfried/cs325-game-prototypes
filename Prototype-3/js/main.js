038ec8389972281f75d769f81b7596fb4decbbbb
deepfriedchickenfried <47087582+deepfriedchickenfried@users.noreply.github.com>
2019-02-13 07:34:05 -0500

    Update main.js

--- Prototype-3/js/main.js
+++ Prototype-3/js/main.js
@@ -10,7 +10,7 @@ window.onload = function() {
     // loading functions to reflect where you are putting the assets.
     // All loading functions will typically all be found inside "preload()".
     
-    var game = new Phaser.Game( 896, 768, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 1096, 768, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
         // Load an image and call it 'logo'.
@@ -41,7 +41,7 @@ window.onload = function() {
     var ROTATION_SPEED = 300;
     var ACCELERATION = 300;
     var MAX_SPEED = 600;
-    var GRAVITY = 100;
+    var GRAVITY = 200;
 
     var spikes;
     var anchors;
@@ -52,7 +52,7 @@ window.onload = function() {
         
         game.physics.startSystem(Phaser.Physics.ARCADE);
         
-        player1 = game.add.sprite(200, 400, 'player');
+        player1 = game.add.sprite(64, 300, 'player');
         game.physics.arcade.enable(player1);
         player1.body.setCircle(24);
         player1.anchor.setTo(0.5,0.5);
@@ -61,7 +61,7 @@ window.onload = function() {
         player1.body.gravity.y = GRAVITY;
         player1.nearestAnchor;
 
-        player2 = game.add.sprite(600, 400, 'player');
+        player2 = game.add.sprite(game.world.width - 64, 300, 'player');
         game.physics.arcade.enable(player2);
         player2.body.setCircle(24);
         player2.anchor.setTo(0.5,0.5);
@@ -72,9 +72,11 @@ window.onload = function() {
 
         crossHair1 = game.add.sprite(400,300, 'crossHair');
         crossHair1.anchor.setTo(0.5,0.5);
+        crossHair1.alpha = 0;
 
         crossHair2 = game.add.sprite(500,300, 'crossHair');
         crossHair2.anchor.setTo(0.5,0.5);
+        crossHair2.alpha = 0;
         
         p2Cursors = game.input.keyboard.createCursorKeys();
         p2Action = game.input.keyboard.createCursorKeys();
@@ -96,63 +98,63 @@ window.onload = function() {
         }
 
         //14 anchor blocks
-        anchors.game.add.group();
+        anchors = game.add.group();
         anchors.enableBody = true;
 
-        var anchorBlock = anchors.create(128, 320, 'anchor');
+        var anchorBlock = anchors.create(228, 320, 'anchor');
         anchorBlock.anchor.setTo(0.5,0.5);
         anchorBlock.body.immovable = true;
         
-        anchorBlock = anchors.create(256, 256, 'anchor');
+        anchorBlock = anchors.create(356, 256, 'anchor');
         anchorBlock.anchor.setTo(0.5,0.5);
         anchorBlock.body.immovable = true;
         
-        anchorBlock = anchors.create(256, 384, 'anchor');
+        anchorBlock = anchors.create(356, 384, 'anchor');
         anchorBlock.anchor.setTo(0.5,0.5);
         anchorBlock.body.immovable = true;
         
-        anchorBlock = anchors.create(320, 128, 'anchor');
+        anchorBlock = anchors.create(420, 128, 'anchor');
         anchorBlock.anchor.setTo(0.5,0.5);
         anchorBlock.body.immovable = true;
         
-        anchorBlock = anchors.create(320, 512, 'anchor');
+        anchorBlock = anchors.create(420, 512, 'anchor');
         anchorBlock.anchor.setTo(0.5,0.5);
         anchorBlock.body.immovable = true;
 
-        anchorBlock = anchors.create(384, 320, 'anchor');
+        anchorBlock = anchors.create(484, 320, 'anchor');
         anchorBlock.anchor.setTo(0.5,0.5);
         anchorBlock.body.immovable = true;
 
-        anchorBlock = anchors.create(448, 192, 'anchor');
+        anchorBlock = anchors.create(548, 192, 'anchor');
         anchorBlock.anchor.setTo(0.5,0.5);
         anchorBlock.body.immovable = true;
 
         //middle bottom 
-        anchorBlock = anchors.create(448, 448, 'anchor');
+        anchorBlock = anchors.create(548, 448, 'anchor');
         anchorBlock.anchor.setTo(0.5,0.5);
         anchorBlock.body.immovable = true;
 
-        anchorBlock = anchors.create(512, 320, 'anchor');
+        anchorBlock = anchors.create(612, 320, 'anchor');
         anchorBlock.anchor.setTo(0.5,0.5);
         anchorBlock.body.immovable = true;
 
-        anchorBlock = anchors.create(576, 128, 'anchor');
+        anchorBlock = anchors.create(676, 128, 'anchor');
         anchorBlock.anchor.setTo(0.5,0.5);
         anchorBlock.body.immovable = true;
 
-        anchorBlock = anchors.create(576, 512, 'anchor');
+        anchorBlock = anchors.create(676, 512, 'anchor');
         anchorBlock.anchor.setTo(0.5,0.5);
         anchorBlock.body.immovable = true;
 
-        anchorBlock = anchors.create(640, 256, 'anchor');
+        anchorBlock = anchors.create(740, 256, 'anchor');
         anchorBlock.anchor.setTo(0.5,0.5);
         anchorBlock.body.immovable = true;
 
-        anchorBlock = anchors.create(640, 384, 'anchor');
+        anchorBlock = anchors.create(740, 384, 'anchor');
         anchorBlock.anchor.setTo(0.5,0.5);
         anchorBlock.body.immovable = true;
 
-        anchorBlock = anchors.create(768, 320, 'anchor');
+        anchorBlock = anchors.create(868, 320, 'anchor');
         anchorBlock.anchor.setTo(0.5,0.5);
         anchorBlock.body.immovable = true;
         
@@ -166,8 +168,43 @@ window.onload = function() {
 
     function update() {
         
-        
+        var spikeCollisionP1 = game.physics.arcade.collide(player1, spikes);
+        var spikeCollisionP2 = game.physics.arcade.collide(player2, spikes);
+        player1.body.angularVelocity = 0;
+        if(player1.grappling === true)
+        {
+            player1.body.gravity.y = 0;
+            
+            
 
+
+        }else
+        {
+            player1.body.gravity.y = GRAVITY;
+            if (p1Left.isDown)
+            {
+                player1.body.angularVelocity = -ROTATION_SPEED;
+            }else if(p1Right.isDown)
+            {
+                player1.body.angularVelocity = ROTATION_SPEED;
+            }
+        }
+
+        player2.body.angularVelocity = 0;
+        if(player2.grappling === false)
+        {
+            player2.body.gravity.y = 0;
+        }else
+        {
+            player2.body.gravity.y = GRAVITY;
+            if(p2Cursors.left.isDown)
+            {
+                player2.body.angularVelocity = -ROTATION_SPEED;
+            } else if(p2Cursors.left.isDown)
+            {
+                player2.body.angularVelocity = -ROTATION_SPEED;
+            }
+        }
 
 
 
